104c49045529278ee0a0c1b8a60f13f2d0f2e2c8
naezith <naezith@users.noreply.github.com>
2019-05-22 16:03:21 +0300

    Update renew_cert.sh

--- cl-node/node/renew_cert.sh
+++ cl-node/node/renew_cert.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/bash
-sudo service nginx stop
-sudo certbot renew --force-renewal --standalone
-sudo cp -a -L /etc/letsencrypt/live/1337.naezith.com/. ~/cert
-sudo service nginx start
-sudo pm2 restart baas-server
+
+copy_cert () {
+  sudo cp -a -L /etc/letsencrypt/live/1337.naezith.com/. ~/cert
+}
+
+if ["$1" = "copy"]; then
+  copy_cert()
+else
+  sudo service nginx stop
+  sudo certbot renew --force-renewal --standalone
+  copy_cert()
+  sudo service nginx start
+  sudo pm2 restart baas-server
+  copy_cert()
+fi
