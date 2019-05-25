26b39b311039de9055a606393f468ca508163279
naezith <naezith@gmail.com>
2019-05-25 20:55:06 +0300

    baas-server does not use cert anymore

--- cl-node/node/renew_cert.sh
+++ cl-node/node/renew_cert.sh
@@ -6,7 +6,4 @@ if [ "$1" != "copy" ]; then
   sudo certbot renew --force-renewal --standalone
 fi
 
-sudo cp -a -L /etc/letsencrypt/live/chainlizard.kmd.dev/. ~/cert
-sudo chmod -R 777 cert
 sudo service nginx start
-sudo pm2 restart baas-server
