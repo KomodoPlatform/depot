f263945e89e9d1464434a91397ee7efa7998aa02
naezith <naezith@users.noreply.github.com>
2020-01-16 13:33:35 +0300

    Update dexp2p.sh

--- cl-node/node/komodod_updates/dexp2p.sh
+++ cl-node/node/komodod_updates/dexp2p.sh
@@ -1,7 +1,7 @@
 # Check if this patch is done
 
 mkdir /home/ubuntu/komodo_update
-wget -qO- https://raw.githubusercontent.com/KomodoPlatform/depot/master/cl-node/node/custom_builds/dexp2p-linux64.tar.gz | tar xvz -C /home/ubuntu/komodo_update
+wget -qO- https://github.com/KomodoPlatform/depot/releases/download/1.0/linux64.tar.gz | tar xvz -C /home/ubuntu/komodo_update
 mv /home/ubuntu/komodo_update/linux64/komodod /home/ubuntu/komodo/src
 mv /home/ubuntu/komodo_update/linux64/komodo-cli /home/ubuntu/komodo/src
 mv /home/ubuntu/komodo_update/linux64/fetch-params.sh /home/ubuntu/komodo/zcutil/
