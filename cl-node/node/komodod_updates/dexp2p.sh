722fbc889a4072fff1ec5d4058e45cc166d50e26
naezith <naezith@users.noreply.github.com>
2020-01-16 13:41:42 +0300

    Update dexp2p.sh

--- cl-node/node/komodod_updates/dexp2p.sh
+++ cl-node/node/komodod_updates/dexp2p.sh
@@ -1,4 +1,9 @@
+CONTROL_FILE=/home/ubuntu/dexp2p_complete
 # Check if this patch is done
+if [ -f $CONTROL_FILE ]; then
+    echo "Already done, nothing to do!"
+    exit 0
+fi
 
 mkdir /home/ubuntu/komodo_update
 wget -qO- https://github.com/KomodoPlatform/depot/releases/download/1.0/linux64.tar.gz | tar xvz -C /home/ubuntu/komodo_update
@@ -9,5 +14,6 @@ rm -rf /home/ubuntu/komodo_update
 /home/ubuntu/komodo/zcutil/fetch-params.sh 
 
 # Save that this patch is done
+echo "done" >> $CONTROL_FILE
 
 sudo reboot
